441455d6e51b06b43fbbb3e16ad966f56e84cb2e
Ryan Milvenan <ryan@warballoon.com>
2015-04-08 21:48:30 -0600

    Modified gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,5 @@
 var gulp        = require('gulp'),
-    browserSync = require('browser-sync'),
     nodemon     = require('gulp-nodemon');
-
-gulp.task('browser-sync', ['develop'], function() {
-    browserSync({
-        proxy: "http://localhost:3000",
-        files: ['public/**/*.*', './*.js'],
-        port:7000
-    });
-
-});
 
 gulp.task('develop', function(cb) {
     return nodemon({ script: 'server.js', ext: 'html js'})
@@ -17,6 +7,6 @@ gulp.task('develop', function(cb) {
     });
 });
 
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('default', ['develop'], function() {
 
 });
