3f386a1921578e703f18b96b07fa8944cc74e097
melissab147 <melissa.j.bica@gmail.com>
2015-04-15 15:41:53 -0600

    updates to sign in and add button

--- src/js/components/sideBar.js
+++ src/js/components/sideBar.js
@@ -8,6 +8,10 @@ var AddSourceForm = require('./addSourceForm');
 var ImportForm = require('./importForm');
 var OverlayTrigger = require('react-bootstrap').OverlayTrigger;
 var Popover = require('react-bootstrap').Popover;
+var Panel = require('react-bootstrap').Panel;
+var ListGroup = require('react-bootstrap').ListGroup;
+var ListGroupItem = require('react-bootstrap').ListGroupItem;
+var Input = require('react-bootstrap').Input;
 
 var SideBar = React.createClass({
     updateExportObj: function(data) {
@@ -23,27 +27,47 @@ var SideBar = React.createClass({
         } else
             this.setState({sourceForm: true});
     },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var email = this.refs.email.getValue();
+        var password = this.refs.password.getValue();
+        if(!email || !password) {
+            return;
+        }
+        socket.emit('sign-in', {email: email, password: password});
+        email = "";
+        password = "";
+    },
     render: function() {
         // var Glyphicon = ReactBootstrap.Glyphicon;
         return (
             <div className="side-bar col-xs-3 text-center">
-                    <Button bsStyle='primary' block>Sign in</Button><br/>
-                    <h4>Share your dashboard template.</h4>
-                    <ButtonGroup vertical className="import-export-buttons">
-                        <ModalTrigger modal={<ImportForm />}>
-                            <Button>Import</Button>
-                        </ModalTrigger>                             
-                        <Button>Export</Button>
-                    </ButtonGroup>
-                    <br/>
-                    // <div className="add-button icon-plus">
-                    //     <a href="#" onClick={this.toggleHidden}></a>
-                    // </div>
-                    <OverlayTrigger trigger='click' placement='right' overlay={<Popover title='Add an API'> Check this info.</Popover>}>
-                        <div className="add-button icon-plus">
-                            <a href="#" onClick={this.toggleHidden}></a>
-                        </div>                      
-                    </OverlayTrigger>                    
+                <Panel bsStyle='primary' collapsable defaultCollapsed header='Sign In'>
+                    <ListGroup fill>
+                        <ListGroupItem>
+                            <form className="sign-in" onSubmit={this.handleSubmit}>
+                                <Input type="text" placeholder="Email" ref="email" />
+                                <Input type="password" class="form-control" id="passwordField" placeholder="Password" ref="password" />
+                                <Input type="submit" value="Sign in" />
+                            </form>
+                        </ListGroupItem>
+                    </ListGroup>
+                </Panel>
+
+                <h4>Share your dashboard template.</h4>
+                <ButtonGroup vertical className="import-export-buttons">
+                    <ModalTrigger modal={<ImportForm />}>
+                        <Button>Import</Button>
+                    </ModalTrigger>                             
+                    <Button>Export</Button>
+                </ButtonGroup>
+                <br/>
+
+                <OverlayTrigger trigger='click' placement='left' overlay={<Popover title='Add an API'> Check this info.<AddSourceForm /></Popover>}>
+                    <div id="scroll" className="add-button icon-plus">
+                        <a href="#"></a>
+                    </div>                      
+                </OverlayTrigger>               
                 
                 {this.state.sourceForm ? <AddSourceForm url={this.props.url} /> : null }
             </div>
