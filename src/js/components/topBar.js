8da8e7e9757df6248bef915757877dabb15ca4bd
Ryan Milvenan <ryanmilvenan@gmail.com>
2015-04-21 11:44:13 -0600

    WIP new user creation

--- src/js/components/topBar.js
+++ src/js/components/topBar.js
@@ -8,18 +8,22 @@ var DisplayCase = require('./displayCase');
 var TopBar = React.createClass({   
     loadTabs: function(data) {
         this.setState({data: data.tabs})
-        socket.emit('sources:populate', {user: this.props.username})
+        var offset = this.state.key - 1;
+        socket.emit('sources:populate', {user: this.props.username, tab: data.tabs[offset]});
+    },
+    handleSelect: function(key) {
+        this.setState({key: key});
     },
     getInitialState: function() {
         socket.on('server:tabs', this.loadTabs)
-        return {data: []}
+        return {data: [], key: 1}
     },
     render: function() {
         var tabIdx = 0;
         var tabs = this.state.data.map(function(tab) {
             tabIdx++;
             return (
-                <TabPane eventKey={tabIdx} tab={tab.title}>
+                <TabPane eventKey={tabIdx} tab={tab.title} > 
                     <DisplayCase url={this.props.url} />
                 </TabPane>
             )
@@ -27,7 +31,7 @@ var TopBar = React.createClass({
         return (
             <div className="col-xs-9">
                 <PageHeader className="title">Homepage 2.0</PageHeader>
-                <TabbedArea defaultActiveKey={1}>
+                <TabbedArea activeKey={this.state.key} onSelect={this.handleSelect}>
                     {tabs}
                 </TabbedArea>                
             </div>
