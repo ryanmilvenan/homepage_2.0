d87cc1378cb5e816ba6f881461cf5e2d9836a6d3
wuzhi1023 <woozhh@gmail.com>
2015-04-26 15:24:13 -0600

    template/file panel

--- src/js/components/importForm.js
+++ src/js/components/importForm.js
@@ -2,12 +2,12 @@ var React = require('react');
 var Button = require('react-bootstrap').Button;
 var Modal = require('react-bootstrap').Modal;
 var Input = require('react-bootstrap').Input;
-var TabbedArea = require('react-bootstrap').TabbedArea;
-var TabPane = require('react-bootstrap').TabPane;
+var PanelGroup = require('react-bootstrap').PanelGroup;
+var Panel = require('react-bootstrap').Panel;
 
 var ImportForm = React.createClass({
   getInitialState: function() {
-    return {template: false};
+    return {template: false, activeKey: '1'};
   },
   importFile: function(e,data) {
     e.preventDefault()
@@ -15,21 +15,26 @@ var ImportForm = React.createClass({
     console.log(this.refs.file.getValue())
     this.props.onRequestHide()
   },  
+  handleSelect: function(activeKey) {
+    this.setState({ activeKey });
+  },
   render: function() {
     return (
       <Modal {...this.props} bsStyle='primary' title='Import from...' animation={false}>
         <div className='modal-body'>
-          <TabbedArea defaultActiveKey={1}>
-            <TabPane eventKey={1} tab='Choose a template'>pictures of templates</TabPane>
-            <TabPane eventKey={2} tab='Upload a file'>
+          <PanelGroup activeKey={this.state.activeKey} onSelect={this.handleSelect} accordion>
+            <Panel header='Choose a template' eventKey='1'>
+              pictures of templates
+            </Panel>
+            <Panel header='Upload a file' eventKey='2'>
               <form>
                   <Input type='file' label='File' ref='file' help='Select a JSON template file.' />
               </form>
               <div className='modal-footer'>
                   <Button onClick={this.importFile}>Load template</Button>
               </div>
-            </TabPane>
-          </TabbedArea>
+            </Panel>
+          </PanelGroup>
         </div>
       </Modal>
     );
